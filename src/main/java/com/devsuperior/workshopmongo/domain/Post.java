cffb5dbc3faef756fc1e7d3cd2a25f063cd06c9f
Marcelo Macharelli <marcelomacharelli@gmail.com>
2024-01-04 12:05:56 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/devsuperior/workshopmongo/domain/Post.java
+++ src/main/java/com/devsuperior/workshopmongo/domain/Post.java
@@ -1,28 +1,33 @@
 package com.devsuperior.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.devsuperior.workshopmongo.dto.AuthorDTO;
+import com.devsuperior.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
-		
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
+
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	public Post() {
-		
+
 	}
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
@@ -65,6 +70,7 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
@@ -73,6 +79,13 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -91,6 +104,4 @@ public class Post implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
 }
